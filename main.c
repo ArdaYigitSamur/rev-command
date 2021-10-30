d6842b30a36991f00468541647dea47846f36294
ArdaYigitSamur <ardayigitsamur@hotmail.com>
2021-10-30 19:29:51 +0200

    rev command in linux

--- main.c
+++ main.c
@@ -2,51 +2,37 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BUFFSIZE 1024
+#define BUFSIZE 1024
 #define INITCAP 8
 
 char **read(FILE *fp, char **lines, int *lSize){
-    int cap=INITCAP;
-    *lSize=0;
+    int cap = INITCAP;
+    *lSize = 0;
     lines=(char **)malloc(sizeof(char *)*cap);
-    lines[*lSize]=malloc(1024);
-    while(fgets(lines[*lSize],BUFFSIZE,fp)!=NULL){
+    lines[*lSize]=malloc(BUFSIZE);
+    while(fgets(lines[*lSize],BUFSIZE,fp)!=NULL){
         if(*lSize==cap){
             cap*=2;
             lines=realloc(lines,sizeof(char *)*cap);
         }
         (*lSize)++;
-        lines[*lSize]=malloc(1024);
+        lines[*lSize]=malloc(BUFSIZE);
     }
     free(lines[*lSize]);
     return lines;
 }
 
 void write(char **lines, int *lSize){
-    int l=0;
-    int cnt=*lSize;
-    for (int i = (*lSize)-1; i >=0 ; i--)
-    {
-        printf("%d ",cnt );
-        if(cnt==*lSize){
-            l=strlen(lines[i]);
-            lines[i][l]='\0';
-            for(int a=l;a>1;a--)
-                printf("%c",lines[i][a] );
-        }
-        cnt--;
-        l=strlen(lines[i]);
-        lines[i][l-1]='\0';
-        l=strlen(lines[i])-2;
-        for(int a=l;a>=0;a--){
-            printf("%c",lines[i][a] );    
-        }
+    int len=0;
+    for(int l=0;l<(*lSize);l++){
+        len = strlen(lines[l])-1;
+        for(int i=len-1;i>=0;i--)
+            printf("%c",lines[l][i]);
         printf("\n");
     }
 }
 
-void reverse(FILE *fp){
-    char **lines=NULL;
+void reverse(FILE *fp, char **lines){
     int lSize;
     lines=read(fp,lines,&lSize);
     write(lines,&lSize);
@@ -54,14 +40,17 @@ void reverse(FILE *fp){
 
 int main(int argc, char *argv[])
 {
-    FILE* fp=NULL;
-    if(argc>1)
-        fp=fopen(argv[1],"r");
+    FILE *filep=NULL;
+    if(argc > 1)
+        filep=fopen(argv[1],"r");
 
-    if(fp==NULL){
-        printf("ERROR READING FILE!!\n");
+    if(filep==NULL){
+        printf("ERROR WHILE READING FILE!!!\n");
         return 1;
     }
-    reverse(fp);
-    fclose(fp);
+    char **lines = NULL;
+    reverse(filep, lines);
+    fclose(filep);
+    free(lines);
+    return 0;
 }
